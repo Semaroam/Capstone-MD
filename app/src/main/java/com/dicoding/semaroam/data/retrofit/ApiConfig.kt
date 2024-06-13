0a8cf9519c2b18c7933a678fe050d9147f0f1b1c
fadhelmohammad <fadhelmohammad43@gmail.com>
2024-06-13 18:38:20 +0700

    update user Logout and Home activity

--- app/src/main/java/com/dicoding/semaroam/data/retrofit/ApiConfig.kt
+++ app/src/main/java/com/dicoding/semaroam/data/retrofit/ApiConfig.kt
@@ -1,5 +1,7 @@
 package com.dicoding.semaroam.data.retrofit
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -8,9 +10,17 @@ object ApiConfig {
     private const val BASE_URL = "https://api-semaroam-zzwmfkvm2q-et.a.run.app/"
 
     fun getApiService(): AuthService {
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
             .build()
         return retrofit.create(AuthService::class.java)
     }
